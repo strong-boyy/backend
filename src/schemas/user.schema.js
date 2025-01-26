d776cb5f75deaaa332847cfd87a54e077057ca53
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-26 17:06:47 +0700

    Add register validation schema for name, email, password and cfPassword

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -13,6 +13,25 @@ class UserSchema {
         .required("Mật khẩu không được để trống"),
     });
   }
+
+  static get registerSchema() {
+    return yup.object().shape({
+      name: yup.string().required("Tên không được để trống"),
+      email: yup
+        .string()
+        .email("Địa chỉ email không chính xác")
+        .required("Địa chỉ email không được để trống"),
+      password: yup
+        .string()
+        .min(8, "Mật khẩu phải từ 8 ký tự")
+        .required("Mật khẩu không được để trống"),
+      cfPassword: yup
+        .string()
+        .min(8, "Mật khẩu phải từ 8 ký tự")
+        .required("Mật khẩu không được để trống")
+        .oneOf([yup.ref("password"), null], "Mật khẩu xác nhận không khớp"),
+    });
+  }
 }
 
 module.exports = UserSchema;

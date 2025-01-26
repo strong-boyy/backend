1c1d1eb0e6527d2ff929d48f36fb60ef6dd2db8b
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-26 16:02:15 +0700

    Add config.js for DB and JWT configuration with environment variables

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,9 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-    app: {
-        port: process.env.APP_PORT,
-    }
-}
+  app: {
+    port: process.env.APP_PORT,
+    jwt: {
+      key: process.env.JWT_KEY || "strong-boyy",
+      accessExpiresIn: process.env.JWT_ACCESS_EXPIRESIN || "1h",
+      refreshExpiresIn: process.env.JWT_REFRESH_EXPIRESIN || "7d",
+    },
+  },
+  db: {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    name: process.env.DB_NAME || "strong-boyy",
+  },
+};
 
 module.exports = config;

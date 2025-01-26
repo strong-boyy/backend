0cc39606d7609aebdd513229f7a0db52c8003348
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-26 17:05:40 +0700

    Add user authentication route for register

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,5 +6,6 @@ const validate = require("../middlewares/validate.middleware");
 const router = express.Router();
 
 router.post("/auth/login", validate(UserSchema.loginSchema), users.login);
+router.post("/auth/register", validate(UserSchema.registerSchema), users.register);
 
 module.exports = router;

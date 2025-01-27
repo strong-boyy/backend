86a009972b4d66a79baa23715166a8f37f16b4b8
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-27 22:06:51 +0700

    implement Google OAuth login flow

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,10 +2,29 @@ const express = require("express");
 const users = require("../controllers/user.controller");
 const UserSchema = require("../schemas/user.schema");
 const validate = require("../middlewares/validate.middleware");
-
+const passport = require("../../src/passport");
 const router = express.Router();
 
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/api/users/auth/google/profile");
+  }
+);
+
+router.get("/auth/google/profile", users.loginByGoogle);
+
 router.post("/auth/login", validate(UserSchema.loginSchema), users.login);
-router.post("/auth/register", validate(UserSchema.registerSchema), users.register);
+router.post(
+  "/auth/register",
+  validate(UserSchema.registerSchema),
+  users.register
+);
 
 module.exports = router;

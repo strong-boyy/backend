99d91277eba97d7a507dc1a86abca6339d37cd5b
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-30 00:39:46 +0700

    Add validation middleware for schema validation with detailed error handling

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,10 +1,15 @@
+const ApiError = require("../api-error");
+
 module.exports = (schema) => async (req, res, next) => {
   try {
-    await schema.validate(req.body, { abortEarly: true });
+    await schema.validate(req.body, { abortEarly: false });
     next();
   } catch (error) {
-    res.status(400).json({
-      errors: error.errors,
+    const formattedErrors = {};
+    error.inner.forEach((err) => {
+      formattedErrors[err.path] = err.message;
     });
+    const apiError = new ApiError(400, formattedErrors);
+    return res.send(apiError.statusCode, apiError);
   }
 };

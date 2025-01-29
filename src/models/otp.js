da27c44dd020c6d2ba50394cdf404d9db639b331
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-01-30 00:43:06 +0700

    Add user and otp modelss

---
+++ src/models/otp.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Otp extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.User, { foreignKey: 'userId' });
+    }
+  }
+  Otp.init({
+    userId: DataTypes.INTEGER,
+    otp: DataTypes.STRING,
+    expiresAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Otp',
+  });
+  return Otp;
+};
---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasOne(models.Otp, { foreignKey: "userId" });
+    }
+  }
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      googleId: DataTypes.STRING,
+      isActived: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};

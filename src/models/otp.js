7fcf519534f475eb0f90d267e24850bbe6f6f3e5
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-02-04 22:50:36 +0700

    Update otp model

--- src/models/otp.js
+++ src/models/otp.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Otp extends Model {
     /**
@@ -10,16 +8,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.User, { foreignKey: 'userId' });
+      this.belongsTo(models.Users, {
+        foreignKey: "userId",
+        targetKey: "id",
+      });
     }
   }
-  Otp.init({
-    userId: DataTypes.INTEGER,
-    otp: DataTypes.STRING,
-    expiresAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Otp',
-  });
+  Otp.init(
+    {
+      userId: DataTypes.INTEGER,
+      otp: DataTypes.STRING,
+      expiresAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Otp",
+    }
+  );
   return Otp;
 };

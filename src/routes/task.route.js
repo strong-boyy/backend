63364136a05618606e9269080afd141174ea2c55
nguyenhaugiang0702 <nguyenhaugiang0702@gmail.com>
2025-02-06 18:46:22 +0700

    Refactor router to use authToken.verifyToken for authentication

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const tasks = require("../controllers/task.controller");
 const taskSchema = require("../schemas/task.schema");
 const validate = require("../middlewares/validate.middleware");
-const verifyToken = require("../middlewares/auth.middleware");
+const authToken = require("../middlewares/auth.middleware");
 
-router.use(verifyToken);
+router.use(authToken.verifyToken);
 router.get("/findAll", tasks.findAll);
 router.get("/findOne/:taskId", tasks.findOne);
 router.post("/create", validate(taskSchema.createTaskSchema), tasks.create);
